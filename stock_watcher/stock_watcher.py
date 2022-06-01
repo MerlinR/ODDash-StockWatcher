683e49a10f21e1f088fc8efc17cd9ebb0e7fb1f4
Merlin Roe <merlin.roe@hotmail.co.uk>
2022-06-01 19:19:59 +0100

    Added basic price gather and requirment file

--- stock_watcher/stock_watcher.py
+++ stock_watcher/stock_watcher.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, Flask, current_app, render_template, request
+from yahoo_fin import stock_info
 
 api = Blueprint("stock_watcher", __name__)
 
 
 @api.route("/", methods=["GET"])
 def watcher():
-    return render_template("stock_watcher/watcher.html")
+    stock = request.args.get("stock", default="", type=str)
+    if not stock:
+        return render_template("stock_watcher/watcher.html", price="Unknown Stock")
+    price = stock_info.get_live_price(stock)
+    return render_template("stock_watcher/watcher.html", price=price)
